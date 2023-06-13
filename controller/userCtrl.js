3dc2e3dd7d08442d791766b013884c1948847ad3
Angelica Lopez <angelica.lopezcap@gmail.com>
2023-06-13 12:51:56 -0500

    Login user ctrl

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,15 +1,33 @@
+const { json } = require('body-parser');
 const User = require('../models/userModel')
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const { generateToken } = require('../config/jwtToken');
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
-  const findUser = await User.findOne({email:email})
+  const findUser = await User.findOne({ email:email })
   if (!findUser) {
     const newUser = await User.create(req.body)
-    res.json(newUser)
+    res.json(newUser);
   } else {
+     throw new Error('User already exists');
+  }
+});
+const loginUserCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //console.log(email, password);
+  // check if user exists or not
+  const findUser = await User.findOne({ email })
+  if(findUser && (await findUser.isPasswordMatched(password))){
     res.json({
-     throw : new Error('User already exists')
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?._id)
     })
+  } else {
+    throw new Error("Invalid Credentials")
   }
 })
-module.exports={createUser}
+module.exports= { createUser, loginUserCtrl }
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -9,7 +9,7 @@ const notFound = (req, res, next) => {
 //Error handler
 const errorHandler=(err, req, res, next) => {
   const statusCode = res.statusCode == 200 ? 500 : res.statusCode
-  req.status(statusCode);
+  res.status(statusCode);
   res.json({
     message: err?.message,
     stack: err?.stack,
